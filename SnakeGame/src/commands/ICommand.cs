fb4747bae4d5adbac95a49a88ec5b5b7c8c5540e
Steven Karatisoglou <lkytiger@outlook.com>
2022-06-16 13:18:56 -0700

    Refactoring and fixed input being processed faster than game.

--- SnakeGame/src/commands/ICommand.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface ICommand
-{
-	void Execute(Game game);
-}
--- SnakeGame/src/objects/Segment.cs
+++
@@ -1,5 +0,0 @@
-﻿public class Segment : GameObject
-{
-	public Symbol Symbol { get; init; }
-	public bool IsHead { get; init; }
-}
